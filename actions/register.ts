14b64a9ee0e31f7975223745128d1f48c7b93e75
Ahmed Bouchefra <techiediaries9@gmail.com>
2024-05-23 22:22:49 +0100

    first commit

---
+++ actions/register.ts
@@ -0,0 +1,32 @@
+"use server"
+import { connectDB } from "@/libs/mongodb";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export const register = async (values: any) => {
+    console.log("Values: ", values);
+
+    const { email, password, name } = values;
+
+    try {
+        await connectDB();
+        const userFound = await User.findOne({ email });
+        if(userFound){
+            return {
+                error: 'Email already exists!'
+            }
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+          name,
+          email,
+          password: hashedPassword,
+        });
+    
+        const savedUser = await user.save();
+
+    }catch(e){
+        console.log(e);
+    }
+}
